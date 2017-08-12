325bc191c069b82fd77abf61bb6e9a94ab3ac1ef
Vladimir Rodchenko <vladimir@aurochses.com>
2017-08-12 22:09:33 +0300

    initial code

---
+++ src/Aurochses.Testing.Mvc/MvcAssert.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Aurochses.Runtime;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Xunit;
+
+namespace Aurochses.Testing.Mvc
+{
+    /// <summary>
+    /// Mvc Assert.
+    /// </summary>
+    public static class MvcAssert
+    {
+        /// <summary>
+        /// Check view result.
+        /// </summary>
+        /// <param name="actionResult">The action result.</param>
+        /// <param name="viewName">Name of the view.</param>
+        /// <param name="model">The model.</param>
+        /// <returns>ViewResult.</returns>
+        public static ViewResult ViewResult(IActionResult actionResult, string viewName = null, object model = null)
+        {
+            var viewResult = Assert.IsType<ViewResult>(actionResult);
+
+            Assert.Equal(viewName, viewResult.ViewName);
+
+            Assert.True(model.ValueEquals(viewResult.Model));
+
+            return viewResult;
+        }
+
+        /// <summary>
+        /// Check view data.
+        /// </summary>
+        /// <param name="viewResult">The view result.</param>
+        /// <param name="items">The items.</param>
+        /// <returns>ViewDataDictionary.</returns>
+        public static ViewDataDictionary ViewData(ViewResult viewResult, IList<KeyValuePair<string, object>> items)
+        {
+            var viewData = viewResult.ViewData;
+
+            Assert.Equal(items, viewData);
+
+            return viewData;
+        }
+
+        /// <summary>
+        /// Check view data.
+        /// </summary>
+        /// <param name="viewResult">The view result.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>ViewDataDictionary.</returns>
+        public static ViewDataDictionary ViewData(ViewResult viewResult, string key, object value)
+        {
+            return ViewData(viewResult, new List<KeyValuePair<string, object>> {new KeyValuePair<string, object>(key, value)});
+        }
+
+        /// <summary>
+        /// Check model state.
+        /// </summary>
+        /// <param name="viewResult">The view result.</param>
+        /// <param name="errors">The errors.</param>
+        /// <returns>ModelStateDictionary.</returns>
+        public static ModelStateDictionary ModelState(ViewResult viewResult, IList<KeyValuePair<string, string>> errors)
+        {
+            var modelState = viewResult.ViewData.ModelState;
+
+            if (errors == null || errors.Count == 0)
+            {
+                Assert.Equal(true, modelState.IsValid);
+
+                Assert.Equal(0, modelState.Count);
+
+                return modelState;
+            }
+
+            Assert.Equal(false, modelState.IsValid);
+
+            Assert.Equal(errors.Count, modelState.Count);
+
+            foreach (var error in errors)
+            {
+                var modelStateEntry = modelState[error.Key];
+
+                Assert.NotNull(modelStateEntry);
+
+                Assert.True(modelStateEntry.Errors.Any(x => x.ErrorMessage == error.Value));
+            }
+
+            return modelState;
+        }
+
+        /// <summary>
+        /// Check model state.
+        /// </summary>
+        /// <param name="viewResult">The view result.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns>ModelStateDictionary.</returns>
+        public static ModelStateDictionary ModelState(ViewResult viewResult, string key, string errorMessage)
+        {
+            return ModelState(viewResult, new List<KeyValuePair<string, string>> { new KeyValuePair<string, string>(key, errorMessage) });
+        }
+
+        /// <summary>
+        /// Check json result.
+        /// </summary>
+        /// <param name="actionResult">The action result.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>JsonResult.</returns>
+        public static JsonResult JsonResult(IActionResult actionResult, object value)
+        {
+            var jsonResult = Assert.IsType<JsonResult>(actionResult);
+
+            Assert.True(value.ValueEquals(jsonResult.Value));
+
+            return jsonResult;
+        }
+
+        /// <summary>
+        /// Check redirect to action result.
+        /// </summary>
+        /// <param name="actionResult">The action result.</param>
+        /// <param name="actionName">Name of the action.</param>
+        /// <param name="controllerName">Name of the controller.</param>
+        /// <param name="routeValues">The route values.</param>
+        /// <returns>RedirectToActionResult.</returns>
+        public static RedirectToActionResult RedirectToActionResult(IActionResult actionResult, string actionName, string controllerName = null, IList<KeyValuePair<string, object>> routeValues = null)
+        {
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(actionResult);
+
+            Assert.Equal(actionName, redirectToActionResult.ActionName);
+            Assert.Equal(controllerName, redirectToActionResult.ControllerName);
+            Assert.Equal(routeValues, redirectToActionResult.RouteValues);
+
+            return redirectToActionResult;
+        }
+
+        /// <summary>
+        /// Check redirect result.
+        /// </summary>
+        /// <param name="actionResult">The action result.</param>
+        /// <param name="returnUrl">The return URL.</param>
+        /// <returns>RedirectResult.</returns>
+        public static RedirectResult RedirectResult(IActionResult actionResult, string returnUrl)
+        {
+            var redirectResult = Assert.IsType<RedirectResult>(actionResult);
+
+            Assert.Equal(returnUrl, redirectResult.Url);
+
+            return redirectResult;
+        }
+
+        /// <summary>
+        /// Check challenge result.
+        /// </summary>
+        /// <param name="actionResult">The action result.</param>
+        /// <param name="authenticationProperties">The authentication properties.</param>
+        /// <param name="provider">The provider.</param>
+        /// <returns>ChallengeResult.</returns>
+        public static ChallengeResult ChallengeResult(IActionResult actionResult, AuthenticationProperties authenticationProperties, string provider)
+        {
+            var challengeResult = Assert.IsType<ChallengeResult>(actionResult);
+
+            Assert.Equal(authenticationProperties, challengeResult.Properties);
+
+            Assert.Contains(provider, challengeResult.AuthenticationSchemes);
+
+            return challengeResult;
+        }
+    }
+}
---
+++ test/Aurochses.Testing.Mvc.Tests/HttpRequestMessageHelpersTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace Aurochses.Testing.Mvc.Tests
+{
+    public class HttpRequestMessageHelpersTests
+    {
+        private readonly Mock<HttpRequestMessage> _mockHttpRequestMessage;
+        private readonly Mock<HttpResponseMessage> _mockHttpResponseMessage;
+
+        public HttpRequestMessageHelpersTests()
+        {
+            _mockHttpRequestMessage = new Mock<HttpRequestMessage>(MockBehavior.Strict);
+            _mockHttpResponseMessage = new Mock<HttpResponseMessage>(MockBehavior.Strict);
+        }
+
+        [Fact]
+        public async Task SetupAsync_Success()
+        {
+            // Arrange
+            var formData = new Dictionary<string, string>
+            {
+                {"firstField", "firstValue"},
+                {"secondField", "secondValue"}
+            };
+
+            const string antiForgeryTokenValue = "AntiForgeryTokenValue";
+            const string responseHtml = "<form><input name=\"__RequestVerificationToken\" type=\"hidden\" value=\"" + antiForgeryTokenValue + "\" /></form>";
+            _mockHttpResponseMessage.Object.Content = new StringContent(responseHtml);
+
+            var responseCookies = new Dictionary<string, string>
+            {
+                {"firstCookie", "firstValue"},
+                {"secondCookie", "secondValue"}
+            };
+
+            _mockHttpResponseMessage.Object.Headers.Add("Set-Cookie", responseCookies.Select(x => $"{x.Key}={x.Value}").ToList());
+
+            var request = _mockHttpRequestMessage.Object;
+
+            // Act
+            await request.SetupAsync(formData, _mockHttpResponseMessage.Object);
+
+            // Assert
+            Assert.IsType<FormUrlEncodedContent>(request.Content);
+
+            var content = await request.Content.ReadAsStringAsync();
+            Assert.Contains($"__RequestVerificationToken={antiForgeryTokenValue}", content);
+            foreach (var item in formData)
+            {
+                Assert.Contains($"{item.Key}={item.Value}", content);
+            }
+
+            var cookies = request.Headers.GetValues("Cookie").ToList();
+            Assert.Equal(responseCookies.Count, cookies.Count);
+            foreach (var item in responseCookies)
+            {
+                Assert.True(cookies.Contains($"{item.Key}={item.Value}"));
+            }
+        }
+    }
+}
